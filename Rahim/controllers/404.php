d49821bfdda65c5b5deffdc045b21c6067571f35
Dmitriy Maslakov <dmitry.maslakov@gmail.com>
2022-02-14 18:12:36 +0200

    initial

---
+++ Rahim/controllers/404.php
@@ -0,0 +1,10 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+<h1>Hi, The requested page does not exist!</h1>
+<h2>Error 404...</h2>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
---
+++ Rahim/controllers/posts.php
@@ -0,0 +1,9 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+<h1>Hi, This is page Posts!</h1>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
